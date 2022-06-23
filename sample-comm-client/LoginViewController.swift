2c3cd91c229a47687df2613f7a099a14b049007d
Anton Baranenko <anton@Antons-MacBook-Air.local>
2022-06-23 13:02:13 +0200

    Initial working version

---
+++ sample-comm-client/LoginViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  sample-comm-client
+//
+//  Created by Anton Baranenko on 01/06/2022.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+class LoginViewController: UIViewController {
+    let db = Firestore.firestore()
+    var user_id: String?
+    var user: UserInfo?
+    
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginSuccessful" {
+            let destination = segue.destination as! ThreadTableViewController
+            destination.user_id = self.user_id
+            destination.user = self.user
+        }
+    }
+    
+    @IBAction func loginClick() {
+        Auth.auth().signIn(withEmail: self.userNameField!.text ?? "", password: self.passwordField!.text ?? "") { [weak self] authResult, error in
+          guard let strongSelf = self else { return }
+            if error != nil {
+                // Show error message
+                let alert = UIAlertController(title: "Could not log in", message: "We could not log you in, please check your password", preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                strongSelf.present(alert, animated: true, completion: nil)
+            }
+            else {
+                // Get the user detailed data
+                let userRef = strongSelf.db.collection("users").document((authResult?.user.uid)!)
+                
+                userRef.getDocument(as: UserInfo.self) { (result) in
+                    switch (result) {
+                    case .success(let user):
+                        strongSelf.user_id = authResult?.user.uid
+                        strongSelf.user = user
+                        strongSelf.performSegue(withIdentifier: "loginSuccessful", sender: strongSelf)
+                    case .failure(let error):
+                        let alert = UIAlertController(title: "Could not log in", message: "You are not registered in the system, please contact an administrator.", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                        strongSelf.present(alert, animated: true, completion: nil)
+                        print(error)
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
